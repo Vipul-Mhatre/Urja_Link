601544ba32b6a89ad5f8332c3f520b742935cb7a
vipul-mhatre <mhatrevipul1518@gmail.com>
2024-11-17 23:22:08 +0530

    chat screen reload issue

--- server/routes/messages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendMessage, getMessages } = require('../controllers/messageController');
-const { authenticateToken } = require('../middleware/auth');
-
-router.post('/send', authenticateToken, sendMessage);
-router.get('/:userId', authenticateToken, getMessages);
-
-module.exports = router;
